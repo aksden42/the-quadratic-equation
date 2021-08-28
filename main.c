341be7d3807dd9b2dc2ca47c9ca8c268ae41cebe
aksden42 <89652737+aksden42@users.noreply.github.com>
2021-08-29 00:05:14 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,22 +5,20 @@
 
 #define TEST_MODE
 
-const int INF = 1e9;
+const int INFINITE_ROOTS_COUNT = 1e9;
 const double PRECISION = 1e-18;
 
 bool isEqual (double x, double y) {
-    if (fabs (x - y) < PRECISION)
-    {
+    if (fabs (x - y) < PRECISION) {
         return true;
     }
     return false;
 }
 
-
 int solveLinearEquation (double b, double c, double *x1, double *x2) {
     if (isEqual (b, 0)) {
         if (isEqual (c, 0)) {
-            return INF;
+            return INFINITE_ROOTS_COUNT;
         }
         return 0;
     }
@@ -28,7 +26,6 @@ int solveLinearEquation (double b, double c, double *x1, double *x2) {
     *x1 = *x2 = -c / b;
     return 1;
 }
-
 
 int solveQadraticEquation (double a, double b, double c, double *x1, double *x2) {
     assert (isfinite (a));
@@ -49,23 +46,20 @@ int solveQadraticEquation (double a, double b, double c, double *x1, double *x2)
     if (isEqual (D, 0)) {
         *x1 = *x2 = (-b) / (2 * a);
         return 1;
-    } else {
-        *x1 = (-b + sqrt (D)) / (2 * a);
-        *x2 = (-b - sqrt (D)) / (2 * a);
-        return 2;
     }
+
+    *x1 = (-b + sqrt (D)) / (2 * a);
+    *x2 = (-b - sqrt (D)) / (2 * a);
+    return 2;
 }
-
-
 
 void clear (){
     while (getchar () != '\n');
 }
 
-
 void readCoefficient (double *pointerToCoefficient, char typeCoefficient) {
     printf ("%c =", typeCoefficient);
-    while(scanf ("%lg", pointerToCoefficient) != 1){
+    while(scanf ("%lg", pointerToCoefficient) != 1) {
         clear ();
         printf ("%c =", typeCoefficient);
     }
@@ -76,7 +70,6 @@ void readCoefficients (double *a, double *b, double *c) {
     readCoefficient (b, 'b');
     readCoefficient (c, 'c');
 }
-
 
 void printFAIL (int numberTest, double a, double b, double c, int CntSolveQadraticEquation, double user_x1, double user_x2, double x1, double x2) {
     printf ("FAIL# %d: a = %lg, b = %lg, c = %lg, correctCountSolves = %d, correctX1 = %.18f, correctX2 = %.18f, x1 = %.18f, x2 = %.18f \n", numberTest, a, b, c, CntSolveQadraticEquation, user_x1, user_x2, x1, x2);
@@ -93,7 +86,7 @@ void testSolveQadraticEquation (int numberTest, double a, double b, double c, in
         printFAIL (numberTest, a, b, c, CntSolveQadraticEquation, user_x1, user_x2, x1, x2);
         return;
     }
-    if (CntSolveQadraticEquation == 0 || CntSolveQadraticEquation == INF){
+    if (CntSolveQadraticEquation == 0 || CntSolveQadraticEquation == INFINITE_ROOTS_COUNT){
         printOK (numberTest);
         return;
     }
@@ -104,7 +97,6 @@ void testSolveQadraticEquation (int numberTest, double a, double b, double c, in
     }
     printFAIL (numberTest, a, b, c, CntSolveQadraticEquation, user_x1, user_x2, x1, x2);
 }
-
 
 void printAns (int CntSolveQadraticEquation, double *x1, double *x2) {
     assert (x1 != x2);
@@ -129,17 +121,14 @@ void printAns (int CntSolveQadraticEquation, double *x1, double *x2) {
     }
 }
 
-
 void allTest () {
     testSolveQadraticEquation (1, 0, 4, 7, 1, -1.75, -1.75);
-    testSolveQadraticEquation (2, 0, 0, 0, INF, 0, 0);
+    testSolveQadraticEquation (2, 0, 0, 0, INFINITE_ROOTS_COUNT, 0, 0);
     testSolveQadraticEquation (3, -3, 0, 75, 2, -5, 5);
     testSolveQadraticEquation (4, -2, 0, 7, 2, -1.870828693386970700, 1.870828693386970700);
     testSolveQadraticEquation (5, 16, -8, 1, 1, 0.25, 0.25);
     testSolveQadraticEquation (6, 9, -6, 2,0, 0, 0);
 }
-
-
 
 int main () {
 #ifdef TEST_MODE
