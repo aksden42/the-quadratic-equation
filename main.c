2edd3d293eac0f8d62743c0a60419466390e783d
aksden42 <89652737+aksden42@users.noreply.github.com>
2021-09-05 21:08:24 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,20 +8,20 @@
 const int INFINITE_ROOTS_COUNT = -1;
 const double PRECISION = 1e-3;
 
-int solveQuadraticEquation (double a, double b, double c, double *x1, double *x2);
-int solveLinearEquation (double b, double c, double *x);
-bool isEqual (double x, double y);
-void printOK (int testNumber);
-void readCoefficients (double *a, double *b, double *c);
-void readCoefficient (double *pointerToCoefficient, char typeCoefficient);
-void clearConsoleBuffer ();
-void printAnswer (int rootsCount, double x1, double x2);
+int  solveQuadraticEquation (double a, double b, double c, double *x1, double *x2);
+int  solveLinearEquation    (double b, double c, double *x);
+bool isEqual                (double x, double y);
+void printOK                (int testNumber);
+void readCoefficients       (double *a, double *b, double *c);
+void readCoefficient        (double *pointerToCoefficient, char typeCoefficient);
+void clearConsoleBuffer     ();
+void printAnswer            (int rootsCount, double x1, double x2);
 
-void testAll ();
+void testAll                    ();
 void testSolveQuadraticEquation (int testNumber, double a, double b, double c,
                                  int correctRootsCount, double correctX1, double correctX2);
-void testSolveLinearEquation (int testNumber, int correctRootsCount, double k, double b, double correctX);
-void testIsEqual (int testNumber, double x, double y, bool correctAnswer);
+void testSolveLinearEquation    (int testNumber, int correctRootsCount, double k, double b, double correctX);
+void testIsEqual                (int testNumber, double x, double y, bool correctAnswer);
 
 int main () {
     #ifdef TEST_MODE
