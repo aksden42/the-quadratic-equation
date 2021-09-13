a89c98193074baceee9cfad3283beb0b855586f5
aksden42 <89652737+aksden42@users.noreply.github.com>
2021-09-14 01:21:43 +0300

    Update testQuadratic.h

--- testQuadratic.h
+++ testQuadratic.h
@@ -1,10 +1,15 @@
 #ifndef TESTQUADRATIC_H_INCLUDED
 #define TESTQUADRATIC_H_INCLUDED
 
+struct IsEqualTest {
+    double x, y;
+    bool correctAnswer;
+};
+
 void testAll                    ();
 void testSolveQuadraticEquation (int testNumber, double a, double b, double c,
                                  int correctRootsCount, double correctX1, double correctX2);
 void testSolveLinearEquation    (int testNumber, int correctRootsCount, double k, double b, double correctX);
-void testIsEqual                (int testNumber, double x, double y, bool correctAnswer);
+void testIsEqual                (int testNumber, IsEqualTest test);
 
 #endif // TESTQUADRATIC_H_INCLUDED
