f964a94c7d85bd895c685bd61eacafe9172e4ff3
aksden42 <89652737+aksden42@users.noreply.github.com>
2021-09-13 23:30:24 +0300

    Update testQuadratic.h

--- testQuadratic.h
+++ testQuadratic.h
@@ -1,7 +1,6 @@
 #ifndef TESTQUADRATIC_H_INCLUDED
 #define TESTQUADRATIC_H_INCLUDED
 
-
 void testAll                    ();
 void testSolveQuadraticEquation (int testNumber, double a, double b, double c,
                                  int correctRootsCount, double correctX1, double correctX2);
