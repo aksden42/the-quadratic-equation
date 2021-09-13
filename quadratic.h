668d7107fae54d482f7cf6f243cf02cc927ec336
aksden42 <89652737+aksden42@users.noreply.github.com>
2021-09-14 00:57:54 +0300

    Update quadratic.h

--- quadratic.h
+++ quadratic.h
@@ -7,10 +7,7 @@ const double PRECISION = 1e-3;
 int  solveQuadraticEquation (double a, double b, double c, double *x1, double *x2);
 int  solveLinearEquation    (double b, double c, double *x);
 bool isEqual                (double x, double y);
-void printOK                (int testNumber);
 void readCoefficients       (double *a, double *b, double *c);
-void readCoefficient        (double *pointerToCoefficient, char typeCoefficient);
-void clearConsoleBuffer     ();
 void printAnswer            (int rootsCount, double x1, double x2);
 
 #endif // QUADRATIC_H_INCLUDED
