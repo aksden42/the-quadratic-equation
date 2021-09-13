fd8fdc58a67a1f54344f608085f73fa31fc659d1
aksden42 <89652737+aksden42@users.noreply.github.com>
2021-09-13 23:30:05 +0300

    Add files via upload

---
+++ testQuadratic.h
@@ -0,0 +1,11 @@
+#ifndef TESTQUADRATIC_H_INCLUDED
+#define TESTQUADRATIC_H_INCLUDED
+
+
+void testAll                    ();
+void testSolveQuadraticEquation (int testNumber, double a, double b, double c,
+                                 int correctRootsCount, double correctX1, double correctX2);
+void testSolveLinearEquation    (int testNumber, int correctRootsCount, double k, double b, double correctX);
+void testIsEqual                (int testNumber, double x, double y, bool correctAnswer);
+
+#endif // TESTQUADRATIC_H_INCLUDED
